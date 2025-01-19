4f81b12de3f75dbe4ac91b3c34577201cc3d3c20
sidisgr8 <sidatmanipal@gmail.com>
2025-01-19 20:28:30 +0530

    add files

---
+++ index.js
@@ -0,0 +1,21 @@
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countpro= " "+ count + " - "
+    saveEl.textContent += countpro
+    
+    
+    console.log(count)
+    count=0
+    countEl.textContent = count
+}
+
+
